89858c4a9ef60d8e77158191c36d4fae1d3266cc
Perfect  Failure <106130232+FHS19085@users.noreply.github.com>
2022-09-02 00:04:42 +0000

    Added folder for ingredients component and added recipe name function to main

--- main.py
+++ main.py
@@ -1,2 +1,34 @@
 import Recipe_name_serving_function
 print(Recipe_name_serving_function)
+
+# Functions
+def Recipe_name(question, error):
+	valid = False
+	while not valid:
+		response = input(question)
+		if response != "":
+			return response
+		else:
+			print(error)
+def Serving_size(question):
+	error = "Invalid serving size"
+	valid = False
+	while not valid:
+		try:
+			response = int(input(question))
+			if response <= 0:
+				print(error)
+			elif response > 50:
+				print(error)
+			else:
+				return response
+		except ValueError:
+			print(error)
+
+
+# Main Routine
+Recipe = Recipe_name("Name of recipe?\n", "Please enter a valid recipe name")
+print("Recipe name\n", Recipe)
+print()
+Servings = Serving_size("Serving size?\n")
+print("Serving size", Servings)
